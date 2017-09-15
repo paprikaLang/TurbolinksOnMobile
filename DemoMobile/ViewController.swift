32a9933319b9ad7c2004ee96d870299e1c309894
paprikaLang <3060866223@qq.com>
2017-09-15 17:40:58 +0800

    Initial Commit

---
+++ DemoMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoMobile
+//
+//  Created by paprika on 2017/9/15.
+//  Copyright © 2017年 paprika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
