33088d3de7632971900a32a4c87781fdf7ef3dbf
paprikaLang <3060866223@qq.com>
2017-09-17 13:32:35 +0800

    呈现

--- DemoMobile/AppDelegate.swift
+++ DemoMobile/AppDelegate.swift
@@ -7,40 +7,73 @@
 //
 
 import UIKit
-
+import Turbolinks
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let navigationController = UINavigationController()
+    
+    let session = Session()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window?.rootViewController = navigationController
+        
+        startApplication()
+        
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func startApplication(){
+    
+        session.delegate = self
+        
+        visit(url:URL(string:"http://localhost:3000/posts")!)
+    
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func visit(url:URL)  {
+        
+        let controller = VisitableViewController(url:url)
+        navigationController.pushViewController(controller, animated: true)
+        session.visit(controller)
+        
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
+
+
+extension AppDelegate : SessionDelegate{
+
+
+    func session(_ session: Session, didProposeVisitToURL URL: URL, withAction action: Action) {
+        visit(url: URL)
+    }
+
+    
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, withError error: NSError) {
+        
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        navigationController.present(alertController, animated: true, completion: nil)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
